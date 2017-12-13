18099977f0b349505696e5c9e9bbe87ca25ec162
fridthjofur12@ru.is <friffi123>
2017-12-13 14:47:49 +0000

    Fix

--- provisioning/create-aws-docker-host-instance.sh
+++ provisioning/create-aws-docker-host-instance.sh
@@ -56,6 +56,6 @@ MY_CIDR=${MY_PUBLIC_IP}/32
 echo Using CIDR ${MY_CIDR} for access restrictions.
 
 set +e
-#aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
-#aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
 
