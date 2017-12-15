fda96058541cf0ee5511f55a25995989f8633b90
fridthjofur12@ru.is <friffi123>
2017-12-15 15:21:42 +0000

    Added column aggregate-id to the event-log table

---
+++ server/migrations/20176112322401-add-column-aggregate-id.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let dbm;
+let type;
+let seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db,callback) {
+  db.addColumn('eventlog', 'aggregate-id', { type: 'string'}, callback);
+};
+
+exports.down = function(db) {
+  db.removeColummn('eventlog', 'aggregate-id', callback);
+};
+
+exports._meta = {
+  "version": 1
+};
